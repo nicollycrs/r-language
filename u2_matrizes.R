5254aa8522cc8d8620912c39b9fadc7fca68ccd5
Nicolly <80932261+nicollycrs@users.noreply.github.com>
2025-03-27 13:01:56 -0300

    Add files via upload

--- u2_matrizes.R
+++ u2_matrizes.R
@@ -106,4 +106,83 @@ m[ , 3] <- c(50,60)
 
 
 
+# Operações e funções
 
+A <- matrix(c(7,2,-5,9,1,0),3,2)
+B <- matrix(c(5,1,8,0,-2,1),3,2)
+
+
+## Concatenação
+
+C <- cbind(A, B) #por coluna cbind = column bind
+
+C <- rbind(A, B) #por linha rbind = row bind
+
+
+vals <- sample(1:50, size=12, replace=FALSE)
+m <- matrix(vals, nrow=4, ncol=3)
+
+
+m[, 2] <- m[, 1] + m[, 3] # criando uma coluna com base na soma das duas outras
+
+
+m[4,] <- m[1,] - m[3,] # criando linha com base na subtração de outras duas
+
+
+m * 10 # multiplica todos os elementos
+
+m + 10 # adiciona 10 em todos os elementos
+
+
+
+# Multiplicação entre matrizes
+
+m <- matrix(5:10, nrow=2, ncol=3)
+n <- matrix(7:12, nrow=3, ncol=2)
+
+m %*% n
+
+
+# Diagonal principal
+
+m <- matrix(c(2,3,-2,1,2,2,4,2,3),3,3)
+
+diag(m)
+
+diag(diag(m))
+
+
+# Matriz transposta
+
+t(m)
+
+# Matriz inversa
+
+m <- matrix(c(4,4,-2,2,6,2,2,8,4),3,3)
+
+invm <- solve(m)
+
+m %*% invm # Matriz identidade
+
+
+# Determinante de uma matriz
+
+det(m)
+
+
+# Soma, Soma de Colunas e Soma e Linhas
+
+sum(m)
+
+colSums(m)
+
+rowSums(m)
+
+
+# Média, Média de Colunas e Média de Linhas
+
+mean(m)
+
+colMeans(m)
+
+rowMeans(m)
